d66531be3b4334526c1fe36af1df1c07a1e781f9
fwuensche <f.wuensche@gmail.com>
2012-07-16 12:08:30 -0300

    Created with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookable::Application.config.secret_token = 'b05b9b5fd83a87b117adb81dd310097557761b720f52127be9b27226460351413e0af0268e6f37432fa45af7929e99b2faf96576071474bf75657ecb278469c0'
