d3af5b107ae934c0b7f54bb1545543269e7766ed
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-20 11:03:53 -0300

    coming back

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,14 +1,18 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "support@innvent.com.br"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifications.forgot_password.subject
   #
-  def forgot_password
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+   def forgot_password(to, login, pass, sent_at = Time.now)
+    @subject    = "Your password is ..."
+    @body['login']=login
+    @body['pass']=pass
+    @recipients = to
+    @from       = 'support@innvent.com.br'
+    @sent_on    = sent_at
+    @headers    = {}
   end
 end
